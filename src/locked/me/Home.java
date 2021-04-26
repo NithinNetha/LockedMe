382ec8d45810f6c6d1d53cd047e87780d9b84462
Nithin Palarapu <nithin6425@gmail.com>
2021-04-26 17:30:12 +0530

    Added 'makeList(), display() methods are created

--- src/locked/me/Home.java
+++ src/locked/me/Home.java
@@ -2,19 +2,39 @@ package locked.me;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 public class Home {
-
+	static String DIRECTORY;
+    File folder_name;
+    List<String> list =new ArrayList<String>(); //Creating a list to store file names
+    
+    
+	public Home() { //constructor
+        DIRECTORY = System.getProperty("user.dir");
+        folder_name = new File(DIRECTORY+"/files");
+        if (!folder_name.exists())
+            folder_name.mkdirs();
+        System.out.println("DIRECTORY : "+ folder_name.getAbsolutePath());
+    }
+	
+	
 	public static void main(String[] args) throws IOException {
-		String line="******Welcome to LockedMe.com*****"+
+		String line="******Welcome to LockedMe.com*****"+  //welcome note
 					"\n***Developed by Nithin Palarapu***";
 		System.out.print(line);
-		Home menu=new Home();
-		menu.mainMenu();
+		Home menu=new Home(); //object creation for class
+		menu.mainMenu(); //Initialization of Main Menu
 	}
+	
+	
+	
 	public void mainMenu() throws IOException{
 		BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
 		int ch=0;
@@ -28,33 +48,47 @@ public class Home {
 				if(ch>3)
 					System.out.println("***Entered option is invalid***\n"); 
 				switch(ch) {
-				case 1: 
-					break;
+				case 1: {
+					list=makeList(); //called to make list 
+					sortList(list); //sort list using selection sort
+					displayList(list); //display list of files
+					break;}
 				case 2:{
-					SubMenu();
+					subMenu(); //calls SubMenu(); function.
 					break;}
 				case 3:
 					System.out.println("Thank you");
-					System.exit(0);
+					System.exit(0);//exit from application
 				}
 			}catch(NumberFormatException e) {
 				System.out.println("\nWrong entry..Enter correct choice");}
 		}while(ch!=3);
 		System.out.println("Thank you");
+	} //closing of mainMenu();
+	private void displayList(List<String> list) {
+		if (list.isEmpty() == true)
+            System.out.println("***There are no files to display***");
+		else {
+			System.out.println("Files currently in directory");
+			for(String i:list)
+				System.out.println(" "+i);
+		}
+		
 	}
-	public void SubMenu(){
+	public void subMenu(){
 		BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
 		int option = 0;
 		do{
 			System.out.println("Choose from below option");
-			System.out.println("\ta.Add file\n\tb.Delete File\n\tc.Search File\n\td.Return to Main Menu");
+			System.out.println("\t1.Add file\n\t2.Delete File\n\t3.Search File\n\t4.Return to Main Menu");
 			try {
 			option=Integer.parseInt(reader.readLine()); //Takes input for option.
 	          switch(option) {
 	          case 1:{
 	        	  System.out.print("Enter file name to create: "); //ask for file name.
 				String filecreate=reader.readLine(); //reads file name.
-				AddFile(filecreate); //AddFile() method is called.  
+				addFile(filecreate); //AddFile() method is called. 
+				list=makeList();
 	          }
 	          case 2:
 	            	break;
@@ -68,9 +102,10 @@ public class Home {
 		}catch (Exception e){
 	            System.out.println("Please enter 1, 2, 3 or 4");}
 		}while(option!=4);
-	}
-	public void AddFile(String name) {  //AddFile definition with name of file
-		File aFile=new File(name); //initializing File class
+	} //Closing of SubMenu(); Function.
+	
+	public void addFile(String name) {  //AddFile definition with name of file
+		File aFile=new File(folder_name +"/"+name); //initializing File class
 		boolean result;
 		if(name.matches("\\S+\\.(.*)")==true) //check if file name is in proper format
 		{
@@ -94,6 +129,39 @@ public class Home {
 		}
 		else
 			System.out.println("Entered name is not a proper format");
-		}
+	} //Closing of addfile() function.
+	
+	public List<String> makeList() {
+		File dir = new File(folder_name +"/"+"."); //giving folder directory
+		List<String> names = Arrays.asList(dir.list(
+		   new FilenameFilter() {
+		      @Override public boolean accept(File dir, String name) {		         	        	 
+		    	  return name.matches("\\S+\\.(.*)"); //checking only for files not folders
+		    	 }
+		   }
+		));
+		return names;
+	}//closing of makeList();
+	
+	//demonstrating Selection sort
+	public void sortList(List<String> list) { 
+		for (int i = 0; i < list.size(); i++) {
+	        String smallest = list.get(i);
+	        int smallestIndex = i;
+	        for (int j = i; j < list.size(); j++) {
+	            String value = list.get(j);
+	            if (value.compareTo(smallest) < 0) {
+	                smallest = value;
+	                smallestIndex = j;
+	            }
+	        }
+
+	        if (smallestIndex != i) {
+	            String head = list.get(i);
+	            list.set(i, smallest);
+	            list.set(smallestIndex, head);
+	        }
+	    }
+	}
 		
 }
